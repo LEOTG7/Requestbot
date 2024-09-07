413f6987a65009001551d37abd951c05ccef60f8
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2024-09-07 21:26:58 +0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
+    FSUB = getenv("FSUB", "justtestmmmu")
     CHID = int(getenv("CHID", "-1000112234"))
     SUDO = list(map(int, getenv("7040444713 2144812475").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://hi:hi@auto.natzh.mongodb.net/?retryWrites=true&w=majority&appName=auto")
