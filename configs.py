5cbccd1ea49f1a13c7e4e4041db6347e3464643c
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2024-09-07 21:23:19 +0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,6 @@ class Config:
     FSUB = getenv("FSUB", "SDBotz")
     CHID = int(getenv("CHID", "-1000112234"))
     SUDO = list(map(int, getenv("7040444713 2144812475").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://hi:hi@auto.natzh.mongodb.net/?retryWrites=true&w=majority&appName=auto")
     
 cfg = Config()
